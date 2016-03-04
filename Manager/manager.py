ab5c99e14839d1ba44bde84cc06f8a7262b00a60
Leandro <lealuque.tw@gmail.com>
2016-03-04 17:39:23 -0300

    New signals

---
+++ Manager/manager.py
@@ -0,0 +1,18 @@
+from PyQt4.QtCore import pyqtSignal
+
+
+
+class Manager(object):
+
+    """
+        Clase que modela el Administrador de Dialogo
+    """
+
+    ##Definiciones de señales para transicionar entre eventos
+    ok = pyqtSignal()
+    cancel = pyqtSignal()
+
+    def __init__(self):
+
+        fsm = None
+        reconocedor = None
